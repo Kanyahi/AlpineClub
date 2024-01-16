99632435be73e57ad6f02a24d69f3dd7687d2500
Hristo Chilov <97700326+Kanyahi@users.noreply.github.com>
2024-01-16 19:27:23 +0200

    pass.js

--- pass.js
+++ pass.js
@@ -1,1 +1,16 @@
+function pass(){
+let password = '052004';
+let picturePass = 'pic2004';
 
+let passwordInput=prompt('Въведете вашата парола!','');
+
+if (passwordInput === password) {
+location.href='/p/blog-page_92.html';
+} else if(passwordInput === picturePass) {
+location.href='/p/blog-page_15.html';
+} else {
+alert('Грешна парола');
+return location.href='/';
+}
+
+}
